6d73518f62208f7e8dc811da812660619bf93c52
Mustapha Anthonio <engamo123@gmail.com>
2023-09-13 07:22:31 +0100

    updated

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const signup = document.getElementById("signup");
-const signin = document.getElementById("signin");
 
 signup.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -22,24 +21,45 @@ signup.addEventListener("submit", function (e) {
     alert("Sign up successful. Please sign in.");
 });
 
-signin.addEventListener("submit", function (e) {
-    e.preventDefault();
-    
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    
-    const storedUser = localStorage.getItem("user");
-    
-    if (storedUser) {
-        const user = JSON.parse(storedUser);
-        if (user.email === email && user.password === password) {
-            alert("Sign in successful. Welcome, " + user.firstname + "!");
+document.addEventListener("DOMContentLoaded", function () {
+    const signin = document.getElementById("signin");
+    signin.addEventListener("submit", function (e) {
+        e.preventDefault();
+        
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        let user = {
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: password,
+          };
+        const storedUser = localStorage.getItem("user");
+        
+        if (storedUser) {
+            const user = JSON.parse(storedUser);
+            if (user.email === email && user.password === password) {
+                alert("Sign in successful. Welcome, " + user.firstname + "!");
+            } else {
+                alert("Incorrect email or password. Please try again.");
+            }
         } else {
-            alert("Incorrect email or password. Please try again.");
+            alert("User not found. Please sign up first.");
         }
-    } else {
-        alert("User not found. Please sign up first.");
-    }
+        
+        signin.reset();
+    });
     
-    signin.reset();
 });
+
+
+function showSignUpForm() {
+    document.getElementById("signup-form").style.display = "block";
+    document.getElementById("signin-form").style.display = "none";
+}
+
+function showSignInForm() {
+    document.getElementById("signup-form").style.display = "none";
+    document.getElementById("signin-form").style.display = "block";
+}
+
